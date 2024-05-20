41daa6e3913113210e7ed090be2bc127c22e83f3
herni86 <cherna1986@gmail.com>
2024-05-19 22:32:08 -0300

    Semana 3

--- Controllers/HelloWorldController1.cs
+++ Controllers/HelloWorldController1.cs
@@ -4,15 +4,19 @@ using System.Text.Encodings.Web;
 namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
         {
-            public string Index()
-            {
-                return "This is my default action...";
-            }
+            return View();
+        }
 
-            public string Welcome(string name, int ID = 1)
-            {
-                return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
-            }
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
     }
+}
+
