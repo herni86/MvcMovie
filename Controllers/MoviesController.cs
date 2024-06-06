fc29f8dbe31fd1239fff5ed9312e4eeeb4309113
herni86 <cherna1986@gmail.com>
2024-06-06 11:04:47 -0300

    Semana 6

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,16 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using MvcMovie.Models;
+using System.Reflection;
 
 namespace MvcMovie.Controllers
 {
     public class MoviesController : Controller
     {
-        public IActionResult Index()
+        private static List<Movie> listMovies = new List<Movie>();
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(listMovies);
+
         }
 
-        // GET: Movies/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -20,18 +23,111 @@ namespace MvcMovie.Controllers
 
             //Simulación de creación de un objeto (model)
             //Mas adelante vamos a ver como usar una base de datos
-            var movie = new Movie
-            {
-                Genre = "Terror",
-                Id = 1,
-                Price = 1,
-                ReleaseDate = DateTime.Now,
-                Title = "La noche del terror"
-            };
+            var movie = listMovies.Find(m => m.Id == id);
 
+            if (movie == null)
+            {
+                return NotFound();
+            }
 
             return View(movie);
         }
-    }
 
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ModelController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Movie model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    // Simplemente asignamos un nuevo id como el siguiente número en la lista
+                    model.Id = listMovies.Count + 1;
+                    listMovies.Add(model);
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, "Error al crear la película: " + ex.Message);
+            }
+            return View(model);
+        }
+
+        // GET: ModelController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var movie = listMovies.Find(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: ModelController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Movie model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var movieIndex = listMovies.FindIndex(m => m.Id == id);
+                    if (movieIndex == -1)
+                    {
+                        return NotFound();
+                    }
+                    listMovies[movieIndex] = model;
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, "Error al editar la película: " + ex.Message);
+            }
+            return View(model);
+        }
+
+        // GET: ModelController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var movie = listMovies.Find(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: ModelController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Movie model)
+        {
+            try
+            {
+                var movieIndex = listMovies.FindIndex(m => m.Id == id);
+                if (movieIndex == -1)
+                {
+                    return NotFound();
+                }
+                listMovies.RemoveAt(movieIndex);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, "Error al eliminar la película: " + ex.Message);
+            }
+            return View(model);
+        }
+    }
 }
+
